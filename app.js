a30284799f2ff186241bb5e357cc8c3ad4ea711e
Code Rat <michael.oshea1@jcu.edu>
2022-11-02 22:42:54 +1000

    implemented the playRound function

--- app.js
+++ app.js
@@ -1,26 +1,97 @@
 
-
+//function which randomly selects computer choice
 function getComputerChoice() {
-    let randNumb =  Math.floor(Math.random() * 3);
-
-    if(randNumb == 0)
+    let x = Math.floor((Math.random() * 3) + 1);
+    result = "";
+    if(x == 1) 
     {
-        return "rock";
+        result = "rock"
+        return result;
     }
-    else if(randNumb == 1)
+    else if(x == 2)
     {
-        return "paper";
+        result = "paper"
+        return result
     }
-    else if(randNumb == 2)
+    else if(x == 3) 
     {
-        return "scissors"
+        result = "scissors"
+        return result
     }
     else
     {
-        return ""
+        result = "Something went wrong"
+        return result
+    }          
+    
+}
+
+//hard coded player selection
+const playerSelection = "rock";
+
+//convert getComputerChoice function to variable in string format
+const computerSelection = getComputerChoice().toString();
+
+//log both variables to the console
+console.log(`Player selection = ${playerSelection}`);
+console.log(`Computer selection = ${computerSelection}`);
+
+//helper function to check if two strings are equal
+function compareString(arg1, arg2) {
+    if(arg1 > arg2)
+    {
+        return 1;
+    }
+    else if(arg1 < arg2)
+    {
+        return -1;
+    }
+    else
+    {
+        return 0;
+    }
+}
+
+//function containing logic about the paper, scissors, rock game
+function playRoundTest(playerSelection, computerSelection) {
+    result = ""
+
+    if((compareString(playerSelection, "rock") == 0)&&(compareString(computerSelection, "scissors") == 0))
+    {
+        result = `You win! ${playerSelection} beats ${computerSelection}.`;
+        return result;
+    }
+    else if((compareString(playerSelection, "rock") == 0)&&(compareString(computerSelection, "paper") == 0))
+    {
+        result = `You lose! ${computerSelection} beats ${playerSelection}.`;
+        return result;
+    }
+    else if((compareString(playerSelection, "paper") == 0)&&(compareString(computerSelection, "rock") == 0))
+    {
+        result = `You win! ${playerSelection} beats ${computerSelection}.`;
+        return result;
+    }
+    else if((compareString(playerSelection, "paper") == 0)&&(compareString(computerSelection, "scissors") == 0))
+    {
+        result = `You lose! ${computerSelection} beats ${playerSelection}.`;
+        return result;
+    }
+    else if((compareString(playerSelection, "scissors") == 0)&&(compareString(computerSelection, "paper") == 0))
+    {
+        result = `You win! ${playerSelection} beats ${computerSelection}.`;
+        return result;
+    }
+    else if((compareString(playerSelection, "scissors") == 0)&&(compareString(computerSelection, "rock") == 0))
+    {
+        result = `You lose! ${computerSelection} beats ${playerSelection}.`;
+        return result;
+    }
+    else
+    {
+        result = `It's a draw! ${computerSelection} draws with ${playerSelection}.`;
+        return result;
     }
 
 }
 
-console.log(getComputerChoice())
-
+console.log(playRoundTest(playerSelection, computerSelection));
