e6c0e37ef4f808ed0ac83c03e14bc118d1570470
Code Rat <michael.oshea1@jcu.edu>
2022-11-05 06:03:18 +1000

    Dunno.

--- app.js
+++ app.js
@@ -27,14 +27,11 @@ function getComputerChoice() {
 }
 
 //hard coded player selection
+//let playerSelection = prompt("Enter your choice: rock, paper or scissors.");
 const playerSelection = "rock";
 
 //convert getComputerChoice function to variable in string format
 const computerSelection = getComputerChoice().toString();
-
-//log both variables to the console
-console.log(`Player selection = ${playerSelection}`);
-console.log(`Computer selection = ${computerSelection}`);
 
 //helper function to check if two strings are equal
 function compareString(arg1, arg2) {
@@ -53,37 +50,44 @@ function compareString(arg1, arg2) {
 }
 
 //function containing logic about the paper, scissors, rock game
-function playRoundTest(playerSelection, computerSelection) {
-    result = ""
+//Maybe the return type of this function 
+function playRound(playerSelection, computerSelection) {
+    result = "";   
 
     if((compareString(playerSelection, "rock") == 0)&&(compareString(computerSelection, "scissors") == 0))
     {
         result = `You win! ${playerSelection} beats ${computerSelection}.`;
+        playerScore += 1;
         return result;
     }
     else if((compareString(playerSelection, "rock") == 0)&&(compareString(computerSelection, "paper") == 0))
     {
         result = `You lose! ${computerSelection} beats ${playerSelection}.`;
+        computerScore += 1;
         return result;
     }
     else if((compareString(playerSelection, "paper") == 0)&&(compareString(computerSelection, "rock") == 0))
     {
         result = `You win! ${playerSelection} beats ${computerSelection}.`;
+        playerScore += 1;
         return result;
     }
     else if((compareString(playerSelection, "paper") == 0)&&(compareString(computerSelection, "scissors") == 0))
     {
         result = `You lose! ${computerSelection} beats ${playerSelection}.`;
+        computerScore += 1;
         return result;
     }
     else if((compareString(playerSelection, "scissors") == 0)&&(compareString(computerSelection, "paper") == 0))
     {
         result = `You win! ${playerSelection} beats ${computerSelection}.`;
+        playerScore += 1;
         return result;
     }
     else if((compareString(playerSelection, "scissors") == 0)&&(compareString(computerSelection, "rock") == 0))
     {
         result = `You lose! ${computerSelection} beats ${playerSelection}.`;
+        computerScore += 1;
         return result;
     }
     else
@@ -94,4 +98,22 @@ function playRoundTest(playerSelection, computerSelection) {
 
 }
 
-console.log(playRoundTest(playerSelection, computerSelection));
+//console.log(playRound(playerSelection, computerSelection));
+
+function game() {
+    playerScore = 0;
+    computerScore = 0;
+
+    for (let i = 0; i < 5; i++) {
+        console.log(`Round ${i+1}`);
+        console.log(`Player selection = ${playerSelection}`);
+        console.log(`Computer selection = ${computerSelection}`);
+        console.log(playRound(playerSelection, computerSelection));
+        console.log(playerScore);
+
+    }
+    
+    
+}
+
+game();
